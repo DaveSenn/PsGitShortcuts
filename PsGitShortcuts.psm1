3a9fe194b7dae6abb1c7b6999b1a59bd02469dbc
Dave Senn <dave.jon.senn@gmail.com>
2016-01-07 13:38:41 +0100

    Add new commands

--- PsGitShortcuts.psm1
+++ PsGitShortcuts.psm1
@@ -12,17 +12,11 @@
 	.EXAMPLE
 		Up "My Message"
 		Pushes all changes to the server using the given commit message.
-	.EXAMPLE
-		Up "My Message" origin/master
-		Up -msg "My Message" -remote origin/master
-		Pushes all changes to the server using the given commit message.
 #>
 function Up() {
-	param(
-		[Parameter(Mandatory=$true)][string]$msg )
+	param([Parameter(Mandatory=$True)][string]$msg )
 		
-	git add -A
-	git commit -m $msg
+	CommitAll $msg
 	git push
 }
 
@@ -59,6 +53,118 @@ function CommitCount() {
 
 <#
     .SYNOPSIS 
+		Snychronizes the current branch with another branch.
+	.Description
+		Snychronizes the current branch with another branch.
+		If a commit message is provided, this command will alsow commit
+		all local changes.
+		
+		1)  Commit everthing if $msg is specified.
+		2)  Check for remote changes.
+		3)  Pull changes.
+		4)  Checkout other branch.
+		5)  Check for remote changes.
+		6)  Pull changes.
+		7)  Merge with current branch.
+		8)  Push.
+		9)  Checkout current branch.
+		10) Merge with target branch.
+		11) Push.
+	.PARAMETER msg
+		The commit message.
+	.PARAMETER targetBranch
+		The target branch.
+	.EXAMPLE
+		SyncBranches master
+		SyncBranches -targetBranch master
+		Synchronizes the current branch with master.
+	.EXAMPLE
+		SyncBranches master "My Message"
+		SyncBranches -targetBranch master -msg "My Message"
+		Commits all changes and synchronizes the current branch with master.
+#>
+function SyncBranches() {
+	param(
+		[Parameter(Mandatory=$true)][string]$targetBranch,
+		[Parameter(Mandatory=$False)][string]$msg)
+	
+	$currentBranch = git name-rev --name-only HEAD
+	if($msg) {
+		CommitAll $msg
+	}
+	
+	$contunue = TryMergeRemoteBranch
+	if($contunue -eq $False) {
+		return;
+	}
+	
+	git checkout $targetBranch
+	$contunue = TryMergeRemoteBranch
+	if($contunue -eq $False) {
+		return;
+	}
+	
+	git merge $currentBranch --no-ff
+	git push
+	git checkout $currentBranch
+	git merge $targetBranch --no-ff
+	git push
+}
+
+<#
+    .SYNOPSIS 
+		Tries to merge the local branch with the matching remote branch.
+		Returns $True if the merge was succefully; otherweise, $False.
+	.Description
+		Tries to merge the local branch with the matching remote branch.
+		Returns $True if the merge was succefully; otherweise, $False.
+	.EXAMPLE
+		TryMergeRemoteBranch
+		Tries to merge the branch.
+#>
+function TryMergeRemoteBranch() {
+	git remote update
+	$result = git status -uno
+	if($result -And $result.Length -gt 1) {
+		if($result[1].Contains("Your branch is up-to-date")) {
+			return $True
+		}
+	}
+	
+	Write-Host "Pending merge detected" -ForegroundColor Yellow -BackgroundColor Black
+	git pull
+	
+	$conflicts = git ls-files -u
+	if($conflicts.Length -ne 0) {
+		Write-Host "Conflicts detected!" -ForegroundColor Red -BackgroundColor Black
+		return $False
+	}
+	
+	return $True
+}
+
+<#
+    .SYNOPSIS 
+		Commits everything to the local repository.
+	.Description
+		1) Add ALL files.
+		2) Commits the changes.
+	.PARAMETER msg
+		The commit message.
+	.EXAMPLE
+		CommitAll "My Message"
+		Commits all changes using the given commit message.
+#>
+function CommitAll() {
+	param(
+		[Parameter(Mandatory=$true)][string]$msg )
+		
+	git add -A
+	git commit -m $msg
+}
+
+<#
+    .SYNOPSIS 
 		Prints the PS git shortcuts help.
 	.Description
 		Prints a basic help text.
@@ -70,6 +176,7 @@ function PsGitHelp() {
 	$projects = @(
 		@{ Name = "Up"; Text = "Add, Commit, Push."; }
 		@{ Name = "CommitCount"; Text = "Print number of commits."; }
+		@{ Name = "SyncBranches"; Text = "Sync the current branch with another branch."; }
 	)
 	
 	$maxLength = 0;
@@ -87,4 +194,4 @@ function PsGitHelp() {
 }
 
 # Export all functions
-Export-ModuleMember -Function *
+Export-ModuleMember -Function Up, CommitCount, SyncBranches, PsGitHelp
